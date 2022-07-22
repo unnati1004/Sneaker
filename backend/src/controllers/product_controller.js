e87f6d1a687ae09bf85c04080198ef08b762c5ff
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-07-22 22:00:59 +0530

    add backend

---
+++ backend/src/controllers/product_controller.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const Prod = require("../models/product")
+
+router.get("", async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const size = req.query.size || 15;
+        const sort = {};
+
+        if (req.query.sortBy && req.query.OrderBy) {
+            sort[req.query.sortBy] = req.query.OrderBy === "desc" ? -1 : 1;
+        }
+        let products
+        if (req.query.price) {
+            products = await Prod.find({ price: { $gte: req.query.price } })
+                .sort(sort)
+                .skip((page - 1) * size)
+                .limit(size)
+                .lean()
+                .exec();
+        }
+        else {
+            products = await Prod.find().skip((page -1)*size).limit(size).sort(sort).lean().exec();
+        }
+
+        return res.status(200).send(products);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+module.exports = router
---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const connect = require('./config/db');
+const cors = require('cors'); 
+const product = require('./controllers/product_controllers')
+app.use(express.json());
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
+app.use('/sneaker',product);
+const port = 2345;
+app.listen(port, async () => {
+  try {
+    await connect();
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log(`listening on port ${port}`);
+});
